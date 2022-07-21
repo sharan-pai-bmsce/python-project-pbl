1924ac39454ce9c04b0bb2a9e67e84c8d4354884
shashank59 <shashanksharma.cs19@bmsce.ac.in>
2022-07-21 13:32:18 +0530

    update-graph-and-integration

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -5,9 +6,19 @@ from tkinter import messagebox
 from forex_python.converter import CurrencyRates
 from forex_python.converter import CurrencyCodes
 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
+from urllib import response
+import requests
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+
+
 root = Tk()
 root.title('Currency Conversion')
-# root.iconbitmap('c:/gui/codemy.ico')
+
 root.geometry("500x500")
 dict1={'US Dollar':'USD','European Euro': 'EUR', 'Japanese yen': 'JPY', 'Bulgarian lev': 'BGN', 'Czech koruna': 'CZK', 'Danish krone': 'DKK', 'British pound': 'GBP', 'Hungarian forint': 'HUF', 'Polish zloty': 'PLN', 'Romanian leu': 'RON', 'Swedish krona': 'SEK', 'Swiss franc': 'CHF', 'Icelandic króna': 'ISK', 'Norwegian krone': 'NOK', 'Croatian kuna': 'HRK', 'Turkish new lira': 'TRY', 'Australian dollar': 'AUD', 'Brazilian real': 'BRL', 'Canadian dollar': 'CAD', 'Chinese/Yuan renminbi': 'CNY', 'Hong Kong dollar': 'HKD', 'Indonesian rupiah': 'IDR', 'Indian rupee': 'INR', 'South Korean won': 'KRW', 'Mexican peso': 'MXN', 'Malaysian ringgit': 'MYR', 'New Zealand dollar': 'NZD', 'Philippine peso': 'PHP', 'Singapore dollar': 'SGD', 'Thai baht': 'THB', 'South African rand': 'ZAR'}
 currencies=[i for i in dict1.keys()]
@@ -20,23 +31,27 @@ my_notebook.pack(pady=5)
 # Create Two Frames
 currency_frame = Frame(my_notebook, width=480, height=480)
 conversion_frame = Frame(my_notebook, width=480, height=480)
+trend_frame = Frame(my_notebook, width=480, height=480)
 
 currency_frame.pack(fill="both", expand=1)
 conversion_frame.pack(fill="both", expand=1)
+trend_frame.pack(fill="both", expand=1)
 
 # Add our Tabs
 my_notebook.add(currency_frame, text="Currencies")
 my_notebook.add(conversion_frame, text="Convert")
-
+my_notebook.add(trend_frame, text="Conversion Trend")
 # Disable 2nd tab
 my_notebook.tab(1, state='disabled')
+my_notebook.tab(2, state='disabled')
 
 #######################
 # CURRENCY STUFF
 #######################
 def lock():
 	# print(,)
-	home=home_selected.get()
+
+	home = home_selected.get()
 	foreign=convert_select.get()
 	if home==foreign:
 		messagebox.showwarning("WARNING!", "Home currency and Foreign Currency are same")	
@@ -48,19 +63,45 @@ def lock():
 		# rate_entry.config(state="disabled")
 		# Enable tab
 		my_notebook.tab(1, state='normal')
+		my_notebook.tab(2, state='normal')
+
+		plot_button = Button(master=trend_frame,
+							 command=plot1(1, dict1[home], dict1[foreign], 365),
+							 height=2,
+							 width=10,
+							 text="Plot")
+		#rate_entry.delete(0,tkinter.END)
+		#rate_entry.insert(0,str(cr.get_rate(dict1[home],dict1[foreign])))
 		# Change Tab Field
 		amount_label.config(text=f'Amount of {home} To Convert To {foreign}')
 		converted_label.config(text=f'Equals This Many {foreign}')
 		convert_button.config(text=f'Convert From {home}')
+		rate_entry.config(state="normal")
+		rate_entry.delete(0, END)
+		conv_rate=round(cr.get_rate(dict1[home], dict1[foreign]),3)
+
+		rate_entry.insert(0, conv_rate)
+		rate_entry.config(state="disabled")
 def unlock():
 	# Enable entry boxes
 	home_entry.config(state="normal")
 	conversion_entry.config(state="normal")
+
 	# rate_entry.config(state="normal")
 	# Disable Tab
+
 	my_notebook.tab(1, state='disabled')
+	my_notebook.tab(2, state='disabled')
+	#clear plot
+	for widgets in trend_frame.winfo_children():
+		widgets.destroy()
+
+
+
+
 home = LabelFrame(currency_frame, text="Your Home Currency")
 home.pack(pady=20)
+
 
 # Home currency entry box
 # home_entry = Entry(home, font=("Helvetica", 24))
@@ -87,13 +128,7 @@ convert_select.set(currencies[0])
 conversion_entry = OptionMenu(conversion,convert_select, *currencies)
 conversion_entry.pack(pady=10, padx=10)
 
-# # rate label
-# rate_label = Label(conversion, text="Current Conversion Rate...")
-# rate_label.pack(pady=10)
 
-# # Rate To Entry
-# rate_entry = Entry(conversion, font=("Helvetica", 24))
-# rate_entry.pack(pady=10, padx=10)
 
 # Button Frame
 button_frame = Frame(currency_frame)
@@ -143,6 +178,26 @@ convert_button = Button(amount_label, text="Convert", command=convert)
 convert_button.pack(pady=20)
 
 # Equals Frame
+rate_label1 = LabelFrame(conversion_frame, text="Rate")
+rate_label1.pack(pady=20)
+
+# rate label
+rate_label = Label(rate_label1, text="Current Conversion Rate...")
+rate_label.pack(pady=10)
+
+# # Rate To Entry
+rate_entry = Entry(rate_label1, font=("Helvetica", 24))
+rate_entry.delete(0,END)
+#conv_rate=cr.get_rate(dict1[home],dict1[foreign])
+#print(conv_rate)
+
+#rate_entry.insert(0,cr.get_rate(dict1[homecur],dict1[foreigncur]))
+rate_entry.pack(pady=10, padx=10)
+rate_entry.config(state="disabled")
+
+
+
+# Equals Frame
 converted_label = LabelFrame(conversion_frame, text="Converted Currency")
 converted_label.pack(pady=20)
 
@@ -157,6 +212,61 @@ clear_button.pack(pady=20)
 # Fake Label for spacing
 spacer = Label(conversion_frame, text="", width=68)
 spacer.pack()
+
+def plot1(amount,currency,converted_currency,amount_of_days):
+	# the figure that will contain the plot
+
+	fig = Figure(figsize=(5, 5), dpi=100)
+
+	today_date = datetime.datetime.now()
+	date_1year = (today_date - datetime.timedelta(days=1 * amount_of_days))
+
+	url = f'https://api.exchangerate.host/timeseries'
+	payload = {'base': currency, 'amount': amount, 'start_date': date_1year.date(), 'end_date': today_date.date()}
+	response = requests.get(url, params=payload)
+	data = response.json()
+
+	currency_history = {}
+	rate_history_array = []
+
+	for item in data['rates']:
+		current_date = item
+		currency_rate = data['rates'][item][converted_currency]
+
+		currency_history[current_date] = [currency_rate]
+		rate_history_array.append(currency_rate)
+
+	pd_data = pd.DataFrame(currency_history).transpose()
+	pd_data.columns = ['Rate']
+	pd.set_option('display.max_rows', None)
+	print(pd_data)
+
+	plt = fig.add_subplot(111)
+	plt.plot(rate_history_array)
+	plt.set_ylabel(f'{amount} {currency} to {converted_currency}')
+	plt.set_xlabel('Days')
+	plt.set_title(f'Current rate for {amount} {currency} to {converted_currency} is {rate_history_array[-1]}')
+	# plt.show()
+
+	# creating the Tkinter canvas
+	# containing the Matplotlib figure
+
+	canvas = FigureCanvasTkAgg(fig,
+							   master=trend_frame)
+	canvas.draw()
+
+	# placing the canvas on the Tkinter window
+	canvas.get_tk_widget().pack()
+
+	# creating the Matplotlib toolbar
+	toolbar = NavigationToolbar2Tk(canvas,
+								   trend_frame)
+	toolbar.update()
+
+	# placing the toolbar on the Tkinter window
+	canvas.get_tk_widget().pack()
+
+
 
 
 root.mainloop()
